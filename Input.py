86ce9726929e5f6240c7f087dbed3c8158e19a85
Annamalai1995 <s.annamalai1995@gmail.com>
2025-02-24 12:21:12 +0530

    Variables Datatypes

---
+++ Input.py
@@ -0,0 +1,9 @@
+#BIO DATA
+Name=input("Enter the name")
+College=input("Enter the college")
+Age=float(input("Enter the Age"))
+Height=float(input("Enter the heights"))
+print("My name is:",Name)
+print("College:",College)
+print("Age is ",Age)
+print("Height is:",Height)
---
+++ Varible.py
@@ -0,0 +1,17 @@
+#DATATYPES:
+Name="SRINIVASAN"
+DOB="0/0/1994"
+Place="Salem"
+Height=6.1
+print(type(Name))
+print(type(DOB))
+print(type(Place))
+print(type(Height))
+
+
+#Prints 
+print("My name is ",Name)
+print("DATE OF BIRTH",DOB)
+print("NATIVE PALCE",Place)
+print("HEIGHT",Height)
+
